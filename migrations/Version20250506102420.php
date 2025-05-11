066478e52b4ff8e79815c463a6f514e7b16f9d91
vahidehmanya <vahide.akhtari@gmail.com>
2025-05-11 12:20:44 +0200

    Add files via upload

---
+++ migrations/Version20250506102420.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250506102420 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            CREATE TABLE bestellung (id INT AUTO_INCREMENT NOT NULL, tisch VARCHAR(255) DEFAULT NULL, bnummer VARCHAR(255) DEFAULT NULL, name VARCHAR(255) DEFAULT NULL, preis DOUBLE PRECISION DEFAULT NULL, status VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            DROP TABLE bestellung
+        SQL);
+    }
+}
